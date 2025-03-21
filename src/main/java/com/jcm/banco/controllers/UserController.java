6ee8ccc054967e05aea0845a8ea24ba760a6b669
julio <juliocesar.melegari@gmail.com>
2025-03-20 23:30:19 -0300

    add controller layer and Swagger

---
+++ src/main/java/com/jcm/banco/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.jcm.banco.controllers;
+
+import com.jcm.banco.entities.User;
+import com.jcm.banco.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate){
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
--- src/main/java/com/jcm/banco/entities/AccountType.java
+++ src/main/java/com/jcm/banco/entities/AccountType.java
@@ -15,4 +15,5 @@ public enum AccountType {
         this.type = type;
     }
 
+
 }
